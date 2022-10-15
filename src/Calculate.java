623ee5aa195ba5f89f123a6063e7810ea45ac555
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-10-15 21:45:37 +0300

    t

--- src/Calculate.java
+++ src/Calculate.java
@@ -15,9 +15,22 @@ class Main {
         String str = scanner.nextLine();
         int num1 = Integer.parseInt(String.valueOf(str.charAt(0)));
         int num2 = Integer.parseInt(String.valueOf(str.charAt(4)));
+        int result;
+        switch (String.valueOf(str.charAt(2))){
+            case "/":
+                result = num1 / num2;
+                break;
+            case  "-":
+                result = num1 - num2;
+                break;
+            case  "*":
+                result = num1 * num2;
+                break;
+            default:
+                result = num1 + num2;
+        }
 
-
-        System.out.println(num1 - num2);
+        System.out.println(result);
         scanner.close();
         return str;
     }

05c9ea55b4b180a9873326a7579baf48c266486e
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-10-15 22:57:49 +0300

    t

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Calculate {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Main main = new Main();
         main.calc();
     }
@@ -9,9 +10,10 @@ public class Calculate {
 
 class Main {
 
-    public static String calc(){
+    public static String calc() throws IOException {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите математическое выражение: ");
+        System.out.print("Введите математическое выражение. Все операдны и математические знаки " +
+                "должны быть отделены друг от друга пробелом: ");
         String str = scanner.nextLine();
         int num1 = Integer.parseInt(String.valueOf(str.charAt(0)));
         int num2 = Integer.parseInt(String.valueOf(str.charAt(4)));
@@ -28,6 +30,11 @@ class Main {
                 break;
             default:
                 result = num1 + num2;
+        }
+        if(num1 <= 0){
+            throw new IOException("Операнд 1 не может быть меньше или равен 0");
+        } else if(num2 <= 0){
+            throw new IOException("Операнд 2 не может быть меньше или равен 0");
         }
         System.out.println(result);
         scanner.close();

2dba917987058a05fc9323d10f43b319e0061a25
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2022-10-15 21:47:06 +0300

    t

--- src/Calculate.java
+++ src/Calculate.java
@@ -29,40 +29,8 @@ class Main {
             default:
                 result = num1 + num2;
         }
-
         System.out.println(result);
         scanner.close();
         return str;
     }
 }
-
-
-
-
-
-
-//class Main {
-//    static int answer;
-//    public static int calc(){
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Введите два числа через enter (арабские, либо римские): ");
-//        int numb1 = scanner.nextInt();
-//        int numb2 = scanner.nextInt();
-//        System.out.println("Введите оператор (можно использовать операторы +, -, *, /: ");
-//        String operator = String.valueOf(scanner.next().charAt(0));
-//        switch (operator) {
-//            case "+": answer = numb1 + numb2;
-//                break;
-//            case  "-": answer = numb1 - numb2;
-//                break;
-//            case "*": answer = numb1 * numb2;
-//                break;
-//            case "/": answer = numb1 / numb2;
-//                break;
-//        }
-//        System.out.printf(numb1 + " " + operator + " " + numb2 + " = " + answer);
-//        return answer;
-//    }
-//}
-
-
